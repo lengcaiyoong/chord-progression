8af0e2a7f02fc9277ea5fb8171dc240fe94692a2
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2023-01-13 22:47:12 +0800

    Update chords.R

--- chords.R
+++ chords.R
@@ -9,9 +9,11 @@ ind <- function(x) {
 	A <- matrix(c(x,2,2,1,2,2,2,1))
 	apply(A,2,cumsum)->A
 	A <- ifelse(A<13,A,A-12)
+	B <- matrix(, nrow = 1, ncol = 8)
 	for (i in 1:8){
-		print(m[A[i]])
+		B[i] <- m[A[i]]
 			   }
+	print(B)
 }
 
 #get C and D chords
